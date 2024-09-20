c715dd476983d2a0a1eb1d0677fb69609e9f1219
sasamb4ik <kmito2004@gmail.com>
2024-09-20 14:38:51 +0300

    first push

---
+++ HW_1/first_task.py
@@ -0,0 +1,43 @@
+class SomeModel:
+
+    @staticmethod
+    def count_vowels(message: str) -> int:
+        '''
+        message: str -> Поданная на вход строка
+        Функция возвращает кол-во гласных в строке message
+        '''
+        vowels_set = {"a", "e", "i", "o", "u"}
+        return sum(1 for char in message if char.lower() in vowels_set)
+
+    def predict(self, message: str) -> float:
+        '''
+        message: str -> Поданная на вход строка, состоящая только из букв Unicode формата
+        Функция возвращает отношение гласных букв в строке message к длине строки message
+        Спорную букву Y за гласную считать не будем (это как Й в русском языке, она не явл-ся гласной).
+        '''
+
+        if not message:
+            raise ValueError("Вы передали пустую строку.")
+
+        if not all(char.isalpha() for char in message):
+            raise ValueError("Неверный входной формат строки. Строка должна состоять только из букв Unicode формата.")
+
+        return round(self.count_vowels(message) / len(message), 3)
+
+
+def predict_message_mood(
+        message: str,
+        bad_thresholds: float = 0.3,
+        good_thresholds: float = 0.8,
+) -> str:
+    model = SomeModel()
+    value = model.predict(message)
+
+    if value < bad_thresholds:
+        return "неуд"
+    elif value > good_thresholds:
+        return "отл"
+    return "норм"
+
+
+print(SomeModel().predict("mimmughffrf"))
---
+++ HW_1/test_first_task.py
@@ -0,0 +1,41 @@
+import unittest
+from first_task import *
+
+class TestFirstTask(unittest.TestCase):
+
+    def setUp(self):
+        self.model = SomeModel()
+        self.empty_str_msg = "Вы передали пустую строку."
+        self.invalid_format_msg = "Неверный входной формат строки. Строка должна состоять только из букв Unicode формата."
+
+    def test_count_vowels(self):
+        self.assertEqual(self.model.count_vowels("hello"), 2)
+        self.assertEqual(self.model.count_vowels("hELlo"), 2)
+        self.assertEqual(self.model.count_vowels(""), 0)
+        self.assertEqual(self.model.count_vowels("rhythm"), 0)
+        self.assertEqual(self.model.count_vowels("aeiouAEIOU"), 10)
+        self.assertEqual(self.model.count_vowels("Hello, world!"), 3)
+
+    def _assert_raises_message(self, message, expected_message):
+        with self.assertRaises(ValueError) as context:
+            self.model.predict(message)
+        self.assertEqual(str(context.exception), expected_message)
+
+    def test_predict(self):
+        self._assert_raises_message("", self.empty_str_msg)
+        self._assert_raises_message("Hello123", self.invalid_format_msg)
+        self._assert_raises_message("Hello,mynameisgleb", self.invalid_format_msg)
+        self._assert_raises_message("    ", self.invalid_format_msg)
+        self.assertAlmostEqual(self.model.predict("aeiou"), 1.0)
+        self.assertAlmostEqual(self.model.predict("HeLLo"), 0.4)
+        self.assertAlmostEqual(self.model.predict("rhythm"), 0.0)
+        self.assertAlmostEqual(self.model.predict("gle"), 0.333)
+        self.assertAlmostEqual(self.model.predict("mimmughffrf"), 0.182)
+
+    def test_predict_message_mood(self):
+        self.assertEqual(predict_message_mood("aeiou"), "отл")
+        self.assertEqual(predict_message_mood("HeLLo"), "норм")
+        self.assertEqual(predict_message_mood("rhytm"), "неуд")
+        self.assertEqual(predict_message_mood("gle"), "норм")
+        self.assertEqual(predict_message_mood("mimmughffrf"), "неуд")
+        self.assertEqual(predict_message_mood("aaaiffoooooeeeffeuuuuu"), "отл")
