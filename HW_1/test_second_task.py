369fb927115d56c36980ae2b15b4e6179424b8e4
sasamb4ik <kmito2004@gmail.com>
2024-09-21 13:54:18 +0300

    3

--- HW_1/test_second_task.py
+++ HW_1/test_second_task.py
@@ -8,33 +8,35 @@ class TestSecondTask(unittest.TestCase):
 
     @patch(
         'builtins.open', new_callable=unittest.mock.mock_open,
-        read_data='This is a mocked line.\nGleb loves big data.\nAnother line with data.\n'
+        read_data='This is a mocked line.\n' \
+                  'Gleb loves big data.\n' \
+                  'Another line with data.\n'
     )
     def test_basics_and_read_file(self, mocked_open: patch):
         result = list(search_file_generator('fake_path', ['data'], ['gleb']))
         self.assertEqual(result, ['Another line with data.'])
         mocked_open.assert_called_once_with('fake_path', 'r', encoding='utf-8')
 
-    @patch(
-        'builtins.open', new_callable=unittest.mock.mock_open, read_data=''
-    )
-    def test_empty_file(self, mocked_open: patch):
-        result = list(search_file_generator('fake_path', ['data'], []))
-        self.assertEqual(result, [])
+    def test_empty_file(self):
+        with patch(
+                'builtins.open', new_callable=unittest.mock.mock_open,
+                read_data=''
+                ):
+            result = list(search_file_generator('fake_path', ['data'], []))
+            self.assertEqual(result, [])
 
-    @patch(
-        'builtins.open', new_callable=unittest.mock.mock_open,
-        read_data='This line has no keywords.\nAnother irrelevant line.'
-    )
-    def test_no_matches(self, mocked_open: patch):
-        result = list(search_file_generator('fake_path', ['keyword'], []))
-        self.assertEqual(result, [])
+    def test_no_matches(self):
+        with patch(
+                'builtins.open', new_callable=unittest.mock.mock_open,
+                read_data='This line has no keywords.\nAnother irrelevant line.'
+                ):
+            result = list(search_file_generator('fake_path', ['keyword'], []))
+            self.assertEqual(result, [])
 
-    @patch(
-        'builtins.open', new_callable=unittest.mock.mock_open,
-        read_data='Line with special character %percent.\nLine without '
-                  'keyword.\n'
-    )
-    def test_special_characters(self, mocked_open: patch):
-        result = list(search_file_generator('fake_path', ['percent'], []))
-        self.assertEqual(result, ['Line with special character %percent.'])
+    def test_special_characters(self):
+        with patch(
+                'builtins.open', new_callable=unittest.mock.mock_open,
+                read_data='Line with special character %percent.\nLine without keyword.\n'
+                ):
+            result = list(search_file_generator('fake_path', ['percent'], []))
+            self.assertEqual(result, ['Line with special character %percent.'])
