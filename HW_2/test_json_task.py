83136a63af6f0eb40228c0d6292490ecc65e9294
sasamb4ik <kmito2004@gmail.com>
2024-09-29 01:11:30 +0300

    f

--- HW_2/test_json_task.py
+++ HW_2/test_json_task.py
@@ -1,6 +1,6 @@
 import unittest
-from json_task import process_json, remove_punctuation
 from unittest.mock import patch, Mock
+from json_task import process_json, remove_punctuation
 
 
 class TestProcessJson(unittest.TestCase):
@@ -8,9 +8,11 @@ class TestProcessJson(unittest.TestCase):
     def setUp(self):
         self.mocked_json = {
             "gleb!": "This is a test!",
-            "hello)": "More tests, here!"
+            "hello)": "More tests, here!",
         }
-        self.mocked_json_string = '{"gleb!": "This is a test!", "hello)": "More tests, here!"}'
+        self.mocked_json_string = (
+            '{"gleb!": "This is a test!", "hello)": "More tests, here!"}'
+        )
         self.required_keys = ["gleb", "hello"]
         self.mocked_callback = Mock()
 
@@ -18,23 +20,26 @@ class TestProcessJson(unittest.TestCase):
         self.assertEqual(remove_punctuation("Hello, World!"), "Hello World")
         self.assertEqual(
             remove_punctuation("выаываы!!яыаэээ?"), "выаываыяыаэээ"
-            )
+        )
         self.assertEqual(remove_punctuation("п"), "п")
         self.assertEqual(remove_punctuation(""), "")
         self.assertEqual(
             remove_punctuation("я ща,с си?жу и #$@*&^ дома!шку"),
-            "я щас сижу и  домашку"
-            )
+            "я щас сижу и  домашку",
+        )
 
-    @patch('first_task.json.loads')
+    @patch("first_task.json.loads")
     def test_process_json_with_matches(self, mocked_json_loads):
         mocked_json_loads.return_value = self.mocked_json
 
         tokens = ["this", "tests"]
 
         process_json(
-            self.mocked_json_string, self.required_keys, tokens, self.mocked_callback
-            )
+            self.mocked_json_string,
+            self.required_keys,
+            tokens,
+            self.mocked_callback,
+        )
 
         mocked_json_loads.assert_called_once_with(self.mocked_json_string)
 
@@ -42,15 +47,19 @@ class TestProcessJson(unittest.TestCase):
         self.mocked_callback.assert_any_call("hello)", "tests")
         self.assertEqual(self.mocked_callback.call_count, 2)
 
-    @patch('first_task.json.loads')
+    @patch("first_task.json.loads")
     def test_process_json_no_matches(self, mocked_json_loads):
         mocked_json_loads.return_value = self.mocked_json
 
         tokens = ["mango", "apple"]
 
-        process_json(self.mocked_json_string, self.required_keys, tokens, self.mocked_callback)
+        process_json(
+            self.mocked_json_string,
+            self.required_keys,
+            tokens,
+            self.mocked_callback,
+        )
 
         mocked_json_loads.assert_called_once_with(self.mocked_json_string)
 
         self.mocked_callback.assert_not_called()
-
