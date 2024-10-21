7919f7fade0f1e99818b8db9f3fd85863726ad28
sasamb4ik <75385932+sasamb4ik@users.noreply.github.com>
2024-10-21 14:04:30 +0300

    Add files via upload

--- 04/test_descriptor_checker.py
+++ 04/test_descriptor_checker.py
@@ -8,61 +8,177 @@ class TestDescriptors(unittest.TestCase):
         self.data = Data(num=1, name="Test", price=100)
 
     def test_init(self):
-        self.assertEqual(self.data.num, 1)
-        self.assertEqual(self.data.name, "Test")
-        self.assertEqual(self.data.price, 100)
+        self.assertEqual(self.data.num, 1, "Инициализация 'num' некорректна")
+        self.assertEqual(
+            self.data.name, "Test", "Инициализация 'name' некорректна"
+        )
+        self.assertEqual(
+            self.data.price, 100, "Инициализация 'price' некорректна"
+        )
 
     def test_set_correct_types(self):
-        self.data.num = 10
-        self.data.name = "Новое значение"
-        self.data.price = 200
-        self.assertEqual(self.data.num, 10)
-        self.assertEqual(self.data.name, "Новое значение")
-        self.assertEqual(self.data.price, 200)
+        try:
+            self.data.num = 10
+            self.assertEqual(
+                self.data.num, 10, "'num' не был присвоен корректно"
+            )
+        except TypeError:
+            self.fail("'num' вызвал TypeError при присвоении корректного типа")
+
+        try:
+            self.data.name = "Новое значение"
+            self.assertEqual(
+                self.data.name,
+                "Новое значение",
+                "'name' не был присвоен корректно",
+            )
+        except TypeError:
+            self.fail("'name' вызвал TypeError при присвоении корректного типа")
+
+        try:
+            self.data.price = 200
+            self.assertEqual(
+                self.data.price, 200, "'price' не был присвоен корректно"
+            )
+        except (TypeError, ValueError):
+            self.fail(
+                "'price' вызвал исключение при присвоении корректного типа"
+            )
 
     def test_set_incorrect_type_num(self):
-        with self.assertRaises(TypeError):
+        """
+        Тут я сохраняю старое значение и далее проверяю, что оно не изменилось
+        """
+        current_value = self.data.num
+        with self.assertRaises(
+            TypeError,
+            msg="'num' не вызвал TypeError при присвоении некорректного типа",
+        ):
             self.data.num = "это строка а не интеджер"
+        self.assertEqual(
+            self.data.num,
+            current_value,
+            "Изменение на невалидное значение изменило 'num'",
+        )
 
     def test_set_incorrect_name(self):
-        with self.assertRaises(TypeError):
+        """
+        Тут я сохраняю старое значение и далее проверяю, что оно не изменилось
+        """
+        current_value = self.data.name
+        with self.assertRaises(
+            TypeError,
+            msg="'name' не вызвал TypeError при присвоении некорректного типа",
+        ):
             self.data.name = 123
+        self.assertEqual(
+            self.data.name,
+            current_value,
+            "Изменение на невалидное значение изменило 'name'",
+        )
 
     def test_set_incorrect_price(self):
-        with self.assertRaises(TypeError):
+        current_value = self.data.price
+        with self.assertRaises(
+            TypeError,
+            msg="'price' не вызвал TypeError при присвоении некорректного типа",
+        ):
             self.data.price = "это не интеджер"
+        self.assertEqual(
+            self.data.price,
+            current_value,
+            "Изменение на невалидное значение изменило 'price'",
+        )
 
     def test_set_negative_price(self):
-        with self.assertRaises(ValueError):
+        current_value = self.data.price
+        with self.assertRaises(
+            ValueError,
+            msg="'price' не вызвал ValueError при присвоении отрицательного значения",
+        ):
             self.data.price = -50
+        self.assertEqual(
+            self.data.price,
+            current_value,
+            "Присвоение отрицательного значения изменило 'price'",
+        )
 
     def test_delete_attribute(self):
-        with self.assertRaises(AttributeError):
+        with self.assertRaises(
+            AttributeError, msg="Удаление 'num' не вызвало AttributeError"
+        ):
             del self.data.num
-        with self.assertRaises(AttributeError):
+        with self.assertRaises(
+            AttributeError, msg="Удаление 'name' не вызвало AttributeError"
+        ):
             del self.data.name
-        with self.assertRaises(AttributeError):
+        with self.assertRaises(
+            AttributeError, msg="Удаление 'price' не вызвало AttributeError"
+        ):
             del self.data.price
 
     def test_descriptor_multiplicity(self):
         data1 = Data(num=2, name="Data1", price=150)
         data2 = Data(num=3, name="Data2", price=250)
-        self.assertEqual(data1.num, 2)
-        self.assertEqual(data1.name, "Data1")
-        self.assertEqual(data1.price, 150)
 
-        self.assertEqual(data2.num, 3)
-        self.assertEqual(data2.name, "Data2")
-        self.assertEqual(data2.price, 250)
+        self.assertEqual(
+            data1.num, 2, "'num' в data1 был инициализирован некорректно"
+        )
+        self.assertEqual(
+            data1.name,
+            "Data1",
+            "'name' в data1 был инициализирован некорректно",
+        )
+        self.assertEqual(
+            data1.price, 150, "'price' в data1 был инициализирован некорректно"
+        )
+
+        self.assertEqual(
+            data2.num, 3, "'num' в data2 был инициализирован некорректно"
+        )
+        self.assertEqual(
+            data2.name,
+            "Data2",
+            "'name' в data2 был инициализирован некорректно",
+        )
+        self.assertEqual(
+            data2.price, 250, "'price' в data2 был инициализирован некорректно"
+        )
 
         data1.num = 4
-        self.assertEqual(data1.num, 4)
-        self.assertEqual(data2.num, 3)
+        data1.name = "UpdatedData1"
+        data1.price = 175
+
+        self.assertEqual(data1.num, 4, "'num' в data1 не был изменен корректно")
+        self.assertEqual(
+            data1.name,
+            "UpdatedData1",
+            "'name' в data1 не был изменен корректно",
+        )
+        self.assertEqual(
+            data1.price, 175, "'price' в data1 не был изменен корректно"
+        )
+
+        self.assertEqual(data2.num, 3, "'num' в data2 изменился некорректно")
+        self.assertEqual(
+            data2.name, "Data2", "'name' в data2 изменился некорректно"
+        )
+        self.assertEqual(
+            data2.price, 250, "'price' в data2 изменился некорректно"
+        )
 
     def test_descriptor_positive_integer(self):
         self.data.price = 500
-        self.assertEqual(self.data.price, 500)
-        with self.assertRaises(ValueError):
+        self.assertEqual(
+            self.data.price, 500, "'price' не был присвоен корректно"
+        )
+        with self.assertRaises(
+            ValueError,
+            msg="'price' не вызвал ValueError при присвоении значения 0",
+        ):
             self.data.price = 0
-        with self.assertRaises(ValueError):
+        with self.assertRaises(
+            ValueError,
+            msg="'price' не вызвал ValueError при присвоении отрицательного значения",
+        ):
             self.data.price = -10
